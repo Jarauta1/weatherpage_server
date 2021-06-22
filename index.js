f0c78f9ce7c7373092ef429079eab401e6b9c08d
jarauta1 <diegojarauta8@gmail.com>
2021-06-22 07:59:47 +0200

    optimized code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-/* const MongoClient = require("mongodb").MongoClient; */
+
 const app = express();
 const cors = require("cors");
 const fetch = require("node-fetch")
@@ -9,27 +9,6 @@ let weather = require("./weather")
 app.use(cors());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json())
-
-
-/* let db; */
-
-/* app.use((req,res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-});
- */
-/* app.get("/weather", function (req,res) {
-
-    fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json')
-    .then((res)=>res.json())
-    .then((data)=>{
-        res.send({data: data})
-     })
- 
- }) */
 
 app.use("/weather", weather)
 
