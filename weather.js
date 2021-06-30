16bf2f67d58b2f5824aeeefb85de96c7c4f41394
jarauta1 <diegojarauta8@gmail.com>
2021-06-30 11:27:14 +0200

    Add info cities lat lon and temp to general map

--- weather.js
+++ weather.js
@@ -5,6 +5,7 @@ const app = express();
 const fetch = require("node-fetch")
 
 let average = 0
+let cityInfo = {}
 
 router.get("/average", function (req,res){
     average = 0
@@ -12,30 +13,51 @@ router.get("/average", function (req,res){
     .then((res)=>res.json())
     .then((data)=>{
         average += data.list[0].main.temp
+        cityInfo.almLat = data.list[0].coord.lat
+        cityInfo.almLon = data.list[0].coord.lon
+        cityInfo.almTemp = data.list[0].main.temp
         fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json')
         .then((res)=>res.json())
         .then((data)=>{
             average += data.list[0].main.temp
+            cityInfo.cadLat = data.list[0].coord.lat
+            cityInfo.cadLon = data.list[0].coord.lon
+            cityInfo.cadTemp = data.list[0].main.temp
             fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json')
             .then((res)=>res.json())
             .then((data)=>{
                 average += data.list[0].main.temp
+                cityInfo.corLat = data.list[0].coord.lat
+                cityInfo.corLon = data.list[0].coord.lon
+                cityInfo.corTemp = data.list[0].main.temp
                 fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json')
                 .then((res)=>res.json())
                 .then((data)=>{
                     average += data.list[0].main.temp
+                    cityInfo.graLat = data.list[0].coord.lat
+                    cityInfo.graLon = data.list[0].coord.lon
+                    cityInfo.graTemp = data.list[0].main.temp
                     fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json')
                     .then((res)=>res.json())
                     .then((data)=>{
                         average += data.list[0].main.temp
+                        cityInfo.hueLat = data.list[0].coord.lat
+                        cityInfo.hueLon = data.list[0].coord.lon
+                        cityInfo.hueTemp = data.list[0].main.temp
                         fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json')
                         .then((res)=>res.json())
                         .then((data)=>{
                             average += data.list[0].main.temp
+                            cityInfo.jaeLat = data.list[0].coord.lat
+                            cityInfo.jaeLon = data.list[0].coord.lon
+                            cityInfo.jaeTemp = data.list[0].main.temp
                             fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json')
                             .then((res)=>res.json())
                             .then((data)=>{
                                 average += data.list[0].main.temp
+                                cityInfo.malLat = data.list[0].coord.lat
+                                cityInfo.malLon = data.list[0].coord.lon
+                                cityInfo.malTemp = data.list[0].main.temp
                                 fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json')
                                 .then((res)=>res.json())
                                 .then((data)=>{
@@ -43,7 +65,10 @@ router.get("/average", function (req,res){
                                     average = average/8
                                     average = average-273.15
                                     average = (average).toFixed(1)
-                                    res.send({average: average})
+                                    cityInfo.sevLat = data.list[0].coord.lat
+                                    cityInfo.sevLon = data.list[0].coord.lon
+                                    cityInfo.sevTemp = data.list[0].main.temp
+                                    res.send({average: average, cityInfo: cityInfo})
                                 })
                             })
                         })
