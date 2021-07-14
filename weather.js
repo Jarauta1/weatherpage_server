59158b8224a7169e691998518719a62e194a7fbe
jarauta1 <diegojarauta8@gmail.com>
2021-07-14 08:52:56 +0200

    promise.all to have all info

--- weather.js
+++ weather.js
@@ -4,50 +4,84 @@ const app = express();
 
 const fetch = require("node-fetch")
 
-async function getData(city) {
-    const res = await fetch(`https://influencity.s3.amazonaws.com/coding-test/${city}.json`)
+async function getData(url) {
+    const res = await fetch(url)
     const data = await res.json()
     return data 
 }
 
 router.get("/almeria", function (req,res) {
-    getData("almeria").then(data =>res.send({data:data})) 
+    getData("https://influencity.s3.amazonaws.com/coding-test/almeria.json").then(data =>res.send({data:data})) 
 })
 
 router.get("/cadiz", function (req,res) {
-    getData("cadiz").then(data =>res.send({data:data})) 
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json").then(data =>res.send({data:data})) 
 })
 
 router.get("/cordoba", function (req,res) {
-    getData("cordoba").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json").then(data =>res.send({data:data}))
 })
 
 router.get("/granada", function (req,res) {
-    getData("granada").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json").then(data =>res.send({data:data}))
 })
 
 router.get("/huelva", function (req,res) {
-    getData("huelva").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json").then(data =>res.send({data:data}))
 })
 
 router.get("/jaen", function (req,res) {
-    getData("jaen").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json").then(data =>res.send({data:data}))
 })
 
 router.get("/malaga", function (req,res) {
-    getData("malaga").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json").then(data =>res.send({data:data}))
 })
 
 router.get("/sevilla", function (req,res) {
-    getData("sevilla").then(data =>res.send({data:data}))
+    getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json").then(data =>res.send({data:data}))
 })
 
 let average = 0
-let cityInfo = {}
+let allInfo = {}
 
 router.get("/average", function (req,res){
+    Promise.all([
+        fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json'),
+      ]).then(async([almeria, granada, malaga, jaen, cordoba, sevilla, huelva, cadiz]) => {
+        const dataAlmeria = await almeria.json();
+        const dataGranada = await granada.json();
+        const dataMalaga = await malaga.json();
+        const dataJaen = await jaen.json();
+        const dataCordoba = await cordoba.json();
+        const dataSevilla = await sevilla.json();
+        const dataHuelva = await huelva.json();
+        const dataCadiz = await cadiz.json();
+        allInfo.almeria = dataAlmeria.list;
+        allInfo.granada = dataGranada.list;
+        allInfo.malaga = dataMalaga.list;
+        allInfo.jaen = dataJaen.list;
+        allInfo.cordoba = dataCordoba.list;
+        allInfo.sevilla = dataSevilla.list;
+        allInfo.huelva = dataHuelva.list;
+        allInfo.cadiz = dataCadiz.list;
+        return [allInfo]
+      })
+      .then((allInfo) => {
+          
+        res.send({data:allInfo})
     
-    fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json')
+      }).catch((err) => {
+        console.log(err);
+      });
+    /* fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json')
     .then((res)=>res.json())
     .then((data)=>{
         average += data.list[0].main.temp
@@ -114,7 +148,7 @@ router.get("/average", function (req,res){
                 })
             })
         })
-     })
+     }) */
 })
 
  module.exports = router;
