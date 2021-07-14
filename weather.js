56649b191f01172255957a7d8046b04ed08ed97b
jarauta1 <diegojarauta8@gmail.com>
2021-07-14 10:26:46 +0200

    finished promise.all and send all data with only one route

--- weather.js
+++ weather.js
@@ -4,7 +4,96 @@ const app = express();
 
 const fetch = require("node-fetch")
 
-async function getData(url) {
+let average = 0
+let allInfo = {}
+let generalMap = {}
+
+router.get("/", function (req,res){
+    Promise.all([
+        fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json'),
+        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json'),
+      ]).then(async([almeria, granada, malaga, jaen, cordoba, sevilla, huelva, cadiz]) => {
+        const dataAlmeria = await almeria.json();
+        const dataGranada = await granada.json();
+        const dataMalaga = await malaga.json();
+        const dataJaen = await jaen.json();
+        const dataCordoba = await cordoba.json();
+        const dataSevilla = await sevilla.json();
+        const dataHuelva = await huelva.json();
+        const dataCadiz = await cadiz.json();
+
+        allInfo.almeria = dataAlmeria;
+        generalMap.almLat = allInfo.almeria.list[0].coord.lat
+        generalMap.almLon = allInfo.almeria.list[0].coord.lon
+        generalMap.almTemp = allInfo.almeria.list[0].main.temp
+        average += allInfo.almeria.list[0].main.temp
+
+        allInfo.granada = dataGranada;
+        generalMap.graLat = allInfo.granada.list[0].coord.lat
+        generalMap.graLon = allInfo.granada.list[0].coord.lon
+        generalMap.graTemp = allInfo.granada.list[0].main.temp
+        average += allInfo.granada.list[0].main.temp
+
+        allInfo.malaga = dataMalaga;
+        generalMap.malLat = allInfo.malaga.list[0].coord.lat
+        generalMap.malLon = allInfo.malaga.list[0].coord.lon
+        generalMap.malTemp = allInfo.malaga.list[0].main.temp
+        average += allInfo.malaga.list[0].main.temp
+
+        allInfo.jaen = dataJaen;
+        generalMap.jaeLat = allInfo.jaen.list[0].coord.lat
+        generalMap.jaeLon = allInfo.jaen.list[0].coord.lon
+        generalMap.jaeTemp = allInfo.jaen.list[0].main.temp
+        average += allInfo.jaen.list[0].main.temp
+
+        allInfo.cordoba = dataCordoba;
+        generalMap.corLat = allInfo.cordoba.list[0].coord.lat
+        generalMap.corLon = allInfo.cordoba.list[0].coord.lon
+        generalMap.corTemp = allInfo.cordoba.list[0].main.temp
+        average += allInfo.cordoba.list[0].main.temp
+
+        allInfo.sevilla = dataSevilla;
+        generalMap.sevLat = allInfo.sevilla.list[0].coord.lat
+        generalMap.sevLon = allInfo.sevilla.list[0].coord.lon
+        generalMap.sevTemp = allInfo.sevilla.list[0].main.temp
+        average += allInfo.sevilla.list[0].main.temp
+
+        allInfo.huelva = dataHuelva;
+        generalMap.hueLat = allInfo.huelva.list[0].coord.lat
+        generalMap.hueLon = allInfo.huelva.list[0].coord.lon
+        generalMap.hueTemp = allInfo.huelva.list[0].main.temp
+        average += allInfo.huelva.list[0].main.temp
+
+        allInfo.cadiz = dataCadiz;
+        generalMap.cadLat = allInfo.cadiz.list[0].coord.lat
+        generalMap.cadLon = allInfo.cadiz.list[0].coord.lon
+        generalMap.cadTemp = allInfo.cadiz.list[0].main.temp
+        average += allInfo.cadiz.list[0].main.temp
+
+        average = average/8
+        average = average-273.15
+        average = (average).toFixed(2)
+
+        return [allInfo,generalMap,average]
+      })
+      .then((data) => {
+          
+        res.send({data:data[0], generalMap: data[1], average: data[2]})
+    
+      }).catch((err) => {
+        console.log(err);
+      });
+})
+
+/* Individual routes */
+
+/* async function getData(url) {
     const res = await fetch(url)
     const data = await res.json()
     return data 
@@ -40,115 +129,6 @@ router.get("/malaga", function (req,res) {
 
 router.get("/sevilla", function (req,res) {
     getData("https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json").then(data =>res.send({data:data}))
-})
-
-let average = 0
-let allInfo = {}
-
-router.get("/average", function (req,res){
-    Promise.all([
-        fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json'),
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json'),
-      ]).then(async([almeria, granada, malaga, jaen, cordoba, sevilla, huelva, cadiz]) => {
-        const dataAlmeria = await almeria.json();
-        const dataGranada = await granada.json();
-        const dataMalaga = await malaga.json();
-        const dataJaen = await jaen.json();
-        const dataCordoba = await cordoba.json();
-        const dataSevilla = await sevilla.json();
-        const dataHuelva = await huelva.json();
-        const dataCadiz = await cadiz.json();
-        allInfo.almeria = dataAlmeria.list;
-        allInfo.granada = dataGranada.list;
-        allInfo.malaga = dataMalaga.list;
-        allInfo.jaen = dataJaen.list;
-        allInfo.cordoba = dataCordoba.list;
-        allInfo.sevilla = dataSevilla.list;
-        allInfo.huelva = dataHuelva.list;
-        allInfo.cadiz = dataCadiz.list;
-        return [allInfo]
-      })
-      .then((allInfo) => {
-          
-        res.send({data:allInfo})
-    
-      }).catch((err) => {
-        console.log(err);
-      });
-    /* fetch('https://influencity.s3.amazonaws.com/coding-test/almeria.json')
-    .then((res)=>res.json())
-    .then((data)=>{
-        average += data.list[0].main.temp
-        cityInfo.almLat = data.list[0].coord.lat
-        cityInfo.almLon = data.list[0].coord.lon
-        cityInfo.almTemp = data.list[0].main.temp
-        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cadiz.json')
-        .then((res)=>res.json())
-        .then((data)=>{
-            average += data.list[0].main.temp
-            cityInfo.cadLat = data.list[0].coord.lat
-            cityInfo.cadLon = data.list[0].coord.lon
-            cityInfo.cadTemp = data.list[0].main.temp
-            fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/cordoba.json')
-            .then((res)=>res.json())
-            .then((data)=>{
-                average += data.list[0].main.temp
-                cityInfo.corLat = data.list[0].coord.lat
-                cityInfo.corLon = data.list[0].coord.lon
-                cityInfo.corTemp = data.list[0].main.temp
-                fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/granada.json')
-                .then((res)=>res.json())
-                .then((data)=>{
-                    average += data.list[0].main.temp
-                    cityInfo.graLat = data.list[0].coord.lat
-                    cityInfo.graLon = data.list[0].coord.lon
-                    cityInfo.graTemp = data.list[0].main.temp
-                    fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/huelva.json')
-                    .then((res)=>res.json())
-                    .then((data)=>{
-                        average += data.list[0].main.temp
-                        cityInfo.hueLat = data.list[0].coord.lat
-                        cityInfo.hueLon = data.list[0].coord.lon
-                        cityInfo.hueTemp = data.list[0].main.temp
-                        fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/jaen.json')
-                        .then((res)=>res.json())
-                        .then((data)=>{
-                            average += data.list[0].main.temp
-                            cityInfo.jaeLat = data.list[0].coord.lat
-                            cityInfo.jaeLon = data.list[0].coord.lon
-                            cityInfo.jaeTemp = data.list[0].main.temp
-                            fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/malaga.json')
-                            .then((res)=>res.json())
-                            .then((data)=>{
-                                average += data.list[0].main.temp
-                                cityInfo.malLat = data.list[0].coord.lat
-                                cityInfo.malLon = data.list[0].coord.lon
-                                cityInfo.malTemp = data.list[0].main.temp
-                                fetch('https://influencity.s3.amazonaws.com/coding-test/step_2_92398987243/sevilla.json')
-                                .then((res)=>res.json())
-                                .then((data)=>{
-                                    average += data.list[0].main.temp
-                                    average = average/8
-                                    average = average-273.15
-                                    average = (average).toFixed(1)
-                                    cityInfo.sevLat = data.list[0].coord.lat
-                                    cityInfo.sevLon = data.list[0].coord.lon
-                                    cityInfo.sevTemp = data.list[0].main.temp
-                                    res.send({average: average, cityInfo: cityInfo})
-                                })
-                            })
-                        })
-                     })
-                })
-            })
-        })
-     }) */
-})
+}) */
 
  module.exports = router;
